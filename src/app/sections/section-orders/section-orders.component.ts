c5eb06a5ee597feaede8f90c9e7b1e28657dec49
David Agam <davidght1@gmail.com>
2024-01-19 19:50:01 +0200

    setup routes

---
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-orders',
+  templateUrl: './section-orders.component.html',
+  styleUrl: './section-orders.component.css'
+})
+export class SectionOrdersComponent {
+
+}
